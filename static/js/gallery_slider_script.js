12b2b76722a64ac31ff770a34666d0f76fd5c3bb
Анастасия Кузьмина <kuzmina_anst@mail.ru>
2024-12-06 11:07:39 +0700

    Настройка сладера галереи под мобильные устройства

--- static/js/gallery_slider_script.js
+++ static/js/gallery_slider_script.js
@@ -1,41 +1,46 @@
 let currentSlide = 0;
-        const slides = document.querySelectorAll(".slide")
-        const dots = document.querySelectorAll('.dot')
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
 
-        const init = (n) => {
-            slides.forEach((slide, index) => {
-                slide.style.display = "none"
-                dots.forEach((dot, index) => {
-                    dot.classList.remove("active")
-                })
-            })
-            slides[n].style.display = "block"
-            dots[n].classList.add("active")
-        }
-        document.addEventListener("DOMContentLoaded", init(currentSlide))
-        const next = () => {
-            currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
-            init(currentSlide)
-        }
+const init = (n) => {
+  slides.forEach((slide, index) => {
+    slide.style.display = "none";
+    dots.forEach((dot) => dot.classList.remove("active"));
+  });
+  slides[n].style.display = "block";
+  dots[n].classList.add("active");
+};
 
-        const prev = () => {
-            currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
-            init(currentSlide)
-        }
+document.addEventListener("DOMContentLoaded", () => init(currentSlide));
 
-        document.querySelector(".next").addEventListener('click', next)
+const next = () => {
+  currentSlide = (currentSlide >= slides.length - 1) ? 0 : currentSlide + 1;
+  init(currentSlide);
+};
 
-        document.querySelector(".prev").addEventListener('click', prev)
+const prev = () => {
+  currentSlide = (currentSlide <= 0) ? slides.length - 1 : currentSlide - 1;
+  init(currentSlide);
+};
+
+document.querySelector(".next").addEventListener("click", next);
+document.querySelector(".prev").addEventListener("click", prev);
+
+setInterval(() => {
+  next();
+}, 5000);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    init(i);
+    currentSlide = i;
+  });
+});
 
 
-        setInterval(() => {
-            next()
-        }, 5000);
+const sliderContainer = document.querySelector('.slide-container');
+const hammer = new Hammer(sliderContainer);
 
-        dots.forEach((dot, i) => {
-            dot.addEventListener("click", () => {
-                console.log(currentSlide)
-                init(i)
-                currentSlide = i
-            })
-        })
+hammer.on('swipeleft', next);
+hammer.on('swiperight', prev);
+
