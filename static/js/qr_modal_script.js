31e8d341567745565e426f77aa3fc9f0c5777e8e
Анастасия Кузьмина <kuzmina_anst@mail.ru>
2025-01-30 14:46:49 +0700

    final edits 2

--- static/js/qr_modal_script.js
+++ static/js/qr_modal_script.js
@@ -1,21 +1,23 @@
 const modal = document.getElementById("modal-qr");
-const openModalLink = document.querySelector("#openModal"); 
+const openModalLinks = document.querySelectorAll(".open-modal"); 
 const closeModalButton = document.getElementById("closeModal");
 
-// Открываем модальное окно при клике на ссылку
-openModalLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Предотвращаем переход по ссылке
-    modal.style.display = "block"; // Показываем модальное окно
+// Добавляем обработчик для всех элементов с классом open-modal
+openModalLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.style.display = "block";
+    });
 });
 
 // Закрываем модальное окно при клике на крестик
 closeModalButton.addEventListener("click", function () {
-    modal.style.display = "none"; // Скрываем модальное окно
+    modal.style.display = "none";
 });
 
 // Закрываем модальное окно при клике вне его области
 window.addEventListener("click", function (event) {
     if (event.target === modal) {
-        modal.style.display = "none"; // Скрываем модальное окно, если клик вне его
+        modal.style.display = "none";
     }
 });
