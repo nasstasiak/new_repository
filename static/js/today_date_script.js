e4ad631d1c331cc5218718c6409a4c2f8988b62d
Анастасия Кузьмина <kuzmina_anst@mail.ru>
2025-02-19 10:44:19 +0700

    скрипт календаря: отправка формы (today_date_script.js)

--- static/js/today_date_script.js
+++ static/js/today_date_script.js
@@ -1,21 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var events = [];
-    console.log(events);
-
+    // Инициализация календаря
     $('#calendar').fullCalendar({
         locale: 'ru',
-        height: 'parent',
-        events: events,
+        events: JSON.parse(document.getElementById('calendar').dataset.events),
+        height: 'auto',
+        contentHeight: 'auto',
+        aspectRatio: 1.35,
         eventRender: function (event, element) {
-            element.addClass(event.css_class);
-
-            element.find('.fc-title').html(event.time + '  ' + event.category + '<br/><b> <a href="' + event.url + '" style="color:white;">' + event.title + '</a>');
-
+            element.css('background-color', 'green');
+            element.find('.fc-title').append('<br/><b>' + event.time + '</b><br/><i>' + event.category + '</i>');
         },
         dayRender: function (date, cell) {
             if (date.isSame(new Date(), "day")) {
-                cell.css("background-color", "yellow"); // Сегодняшняя дата выделена желтым
+                cell.css("background-color", "yellow");
             }
+        }
+    });
+
+    // Обработчик для кнопки "Применить"
+    document.getElementById('apply-date-button').addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const year = document.getElementById('yearSelect').value;
+        const month = document.getElementById('monthSelect').value;
+
+        if (year && month) {
+            $('#calendar').fullCalendar('gotoDate', `${year}-${month}-01`);
         }
     });
 });
